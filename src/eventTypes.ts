79f0c2c9dddaf9cd5c1d4ea8dee14bc5be8083d6
Moody Salem <moody.salem@gmail.com>
2025-01-18 10:39:18 -0500

    improve the type extractor

--- src/eventTypes.ts
+++ src/eventTypes.ts
@@ -2,52 +2,46 @@ import type {
   ExtractAbiEvent,
   ExtractAbiEventNames,
   AbiParameterToPrimitiveType,
+  Abi,
 } from "abitype";
 import { CORE_ABI, ORACLE_ABI, POSITIONS_ABI } from "./abis";
 
-type CoreEventNames = ExtractAbiEventNames<typeof CORE_ABI>;
-
-type CoreContractEvent<N extends CoreEventNames> = {
-  [P in ExtractAbiEvent<typeof CORE_ABI, N>["inputs"][number] as P extends {
+type ContractEvent<abi extends Abi, N extends ExtractAbiEventNames<abi>> = {
+  [P in ExtractAbiEvent<abi, N>["inputs"][number] as P extends {
     name: infer N extends string;
   }
     ? N
     : never]: AbiParameterToPrimitiveType<P>;
 };
 
-type PositionEventNames = ExtractAbiEventNames<typeof POSITIONS_ABI>;
-
-type PositionContractEvent<N extends PositionEventNames> = {
-  [P in ExtractAbiEvent<
-    typeof POSITIONS_ABI,
-    N
-  >["inputs"][number] as P extends {
-    name: infer N extends string;
-  }
-    ? N
-    : never]: AbiParameterToPrimitiveType<P>;
-};
-
-type OracleEventNames = ExtractAbiEventNames<typeof ORACLE_ABI>;
-
-type OracleContractEvent<N extends OracleEventNames> = {
-  [P in ExtractAbiEvent<typeof ORACLE_ABI, N>["inputs"][number] as P extends {
-    name: infer N extends string;
-  }
-    ? N
-    : never]: AbiParameterToPrimitiveType<P>;
-};
-
-export type PoolKey = CoreContractEvent<"PositionUpdated">["poolKey"];
-export type CorePositionUpdated = CoreContractEvent<"PositionUpdated">;
-export type CorePoolInitialized = CoreContractEvent<"PoolInitialized">;
-export type CorePositionFeesCollected =
-  CoreContractEvent<"PositionFeesCollected">;
-export type CoreProtocolFeesPaid = CoreContractEvent<"ProtocolFeesPaid">;
-export type CoreProtocolFeesWithdrawn =
-  CoreContractEvent<"ProtocolFeesWithdrawn">;
-export type CoreFeesAccumulated = CoreContractEvent<"FeesAccumulated">;
-export type CoreSwapped = CoreContractEvent<"Swapped">;
-// export type CoreLoadedBalance = CoreContractEvent<"LoadedBalance">;
-export type PositionTransfer = PositionContractEvent<"Transfer">;
-export type SnapshotEvent = OracleContractEvent<"SnapshotEvent">;
+export type PoolKey = ContractEvent<
+  typeof CORE_ABI,
+  "PositionUpdated"
+>["poolKey"];
+export type CorePositionUpdated = ContractEvent<
+  typeof CORE_ABI,
+  "PositionUpdated"
+>;
+export type CorePoolInitialized = ContractEvent<
+  typeof CORE_ABI,
+  "PoolInitialized"
+>;
+export type CorePositionFeesCollected = ContractEvent<
+  typeof CORE_ABI,
+  "PositionFeesCollected"
+>;
+export type CoreProtocolFeesPaid = ContractEvent<
+  typeof CORE_ABI,
+  "ProtocolFeesPaid"
+>;
+export type CoreProtocolFeesWithdrawn = ContractEvent<
+  typeof CORE_ABI,
+  "ProtocolFeesWithdrawn"
+>;
+export type CoreFeesAccumulated = ContractEvent<
+  typeof CORE_ABI,
+  "FeesAccumulated"
+>;
+export type CoreSwapped = ContractEvent<typeof CORE_ABI, "Swapped">;
+export type PositionTransfer = ContractEvent<typeof POSITIONS_ABI, "Transfer">;
+export type SnapshotEvent = ContractEvent<typeof ORACLE_ABI, "SnapshotEvent">;
