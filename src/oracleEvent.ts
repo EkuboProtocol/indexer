4d47d83977b698aeacbf55d9184221429ea17ea3
Moody Salem <moody.salem@gmail.com>
2025-03-16 17:21:24 -0400

    bump to optimized oracle

--- src/oracleEvent.ts
+++ src/oracleEvent.ts
@@ -11,18 +11,18 @@ export interface OracleEvent {
 export function parseOracleEvent(data: `0x${string}`): OracleEvent {
   let n = BigInt(data);
 
-  // tickCumulative: int64 (8 bytes)
-  const tickCumulativeRaw = n & ((1n << 64n) - 1n);
-  const tickCumulative = toSigned(tickCumulativeRaw, 64);
-  n >>= 64n;
+  // timestamp: uint32 (4 bytes)
+  const timestamp = BigInt(n & ((1n << 32n) - 1n));
+  n >>= 32n;
 
   // secondsPerLiquidityCumulative: uint160 (20 bytes)
   const secondsPerLiquidityCumulative = n & ((1n << 160n) - 1n);
   n >>= 160n;
 
-  // timestamp: uint64 (8 bytes)
-  const timestamp = BigInt(n & ((1n << 64n) - 1n));
-  n >>= 32n;
+  // tickCumulative: int64 (8 bytes)
+  const tickCumulativeRaw = n & ((1n << 64n) - 1n);
+  const tickCumulative = toSigned(tickCumulativeRaw, 64);
+  n >>= 64n;
 
   // token: address (20 bytes)
   const tokenBigInt = n & ((1n << 160n) - 1n);
