1c31504db75911ac3c77560d0d2374ceba0de549
Moody Salem <moody.salem@gmail.com>
2025-11-02 14:11:10 +0800

    improve the pool states code

--- migrations/004_pool_states.sql
+++ migrations/004_pool_states.sql
@@ -1,9 +1,9 @@
 CREATE TABLE pool_states (
-	pool_key_id int8 PRIMARY KEY REFERENCES pool_keys (pool_key_id) ON DELETE CASCADE,
-	sqrt_ratio numeric,
-	tick int4,
-	liquidity numeric,
-	last_event_id int8,
+	pool_key_id int8 PRIMARY KEY REFERENCES pool_keys (pool_key_id),
+	sqrt_ratio numeric NOT NULL,
+	tick int4 NOT NULL,
+	liquidity numeric NOT NULL,
+	last_event_id int8 NOT NULL,
 	last_liquidity_update_event_id int8
 );
 
@@ -99,56 +99,14 @@ END;
 $$
 LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION refresh_pool_state_from_position_updates ()
-	RETURNS TRIGGER
-	AS $$
-DECLARE
-	v_pool_key_id int8;
-BEGIN
-	IF TG_OP = 'DELETE' THEN
-		v_pool_key_id := OLD.pool_key_id;
-	ELSE
-		v_pool_key_id := NEW.pool_key_id;
-	END IF;
-	IF v_pool_key_id IS NOT NULL THEN
-		PERFORM
-			refresh_pool_state (v_pool_key_id);
-	END IF;
-	RETURN NULL;
-END;
-$$
-LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION refresh_pool_state_from_swaps ()
-	RETURNS TRIGGER
-	AS $$
-DECLARE
-	v_pool_key_id int8;
-BEGIN
-	IF TG_OP = 'DELETE' THEN
-		v_pool_key_id := OLD.pool_key_id;
-	ELSE
-		v_pool_key_id := NEW.pool_key_id;
-	END IF;
-	IF v_pool_key_id IS NOT NULL THEN
-		PERFORM
-			refresh_pool_state (v_pool_key_id);
-	END IF;
-	RETURN NULL;
-END;
-$$
-LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION refresh_pool_state_from_pool_initializations ()
+CREATE OR REPLACE FUNCTION refresh_pool_state ()
 	RETURNS TRIGGER
 	AS $$
 BEGIN
 	IF TG_OP = 'DELETE' THEN
-		PERFORM
-			refresh_pool_state (OLD.pool_key_id);
+		PERFORM refresh_pool_state (OLD.pool_key_id);
 	ELSE
-		PERFORM
-			refresh_pool_state (NEW.pool_key_id);
+		PERFORM refresh_pool_state (NEW.pool_key_id);
 	END IF;
 	RETURN NULL;
 END;
@@ -156,93 +114,16 @@ $$
 LANGUAGE plpgsql;
 
 CREATE TRIGGER maintain_pool_state_from_position_updates
-	AFTER INSERT OR UPDATE OR DELETE ON position_updates
+	AFTER INSERT OR DELETE ON position_updates
 	FOR EACH ROW
-	EXECUTE FUNCTION refresh_pool_state_from_position_updates ();
+	EXECUTE FUNCTION refresh_pool_state ();
 
 CREATE TRIGGER maintain_pool_state_from_swaps
-	AFTER INSERT OR UPDATE OR DELETE ON swaps
+	AFTER INSERT OR DELETE ON swaps
 	FOR EACH ROW
-	EXECUTE FUNCTION refresh_pool_state_from_swaps ();
+	EXECUTE FUNCTION refresh_pool_state ();
 
 CREATE TRIGGER maintain_pool_state_from_pool_initializations
-	AFTER INSERT OR UPDATE OR DELETE ON pool_initializations
+	AFTER INSERT OR DELETE ON pool_initializations
 	FOR EACH ROW
-	EXECUTE FUNCTION refresh_pool_state_from_pool_initializations ();
-
-INSERT INTO pool_states (pool_key_id, sqrt_ratio, tick, liquidity, last_event_id, last_liquidity_update_event_id)
-SELECT
-	pool_key_id,
-	sqrt_ratio,
-	tick,
-	liquidity,
-	last_event_id,
-	last_liquidity_update_event_id
-FROM ( WITH lss AS (
-		SELECT
-			pk.pool_key_id,
-			coalesce(last_swap.event_id, pi.event_id) AS last_swap_event_id,
-			coalesce(last_swap.sqrt_ratio_after, pi.sqrt_ratio) AS sqrt_ratio,
-			coalesce(last_swap.tick_after, pi.tick) AS tick,
-			coalesce(last_swap.liquidity_after, 0::numeric) AS liquidity_last
-		FROM
-			pool_keys pk
-		LEFT JOIN LATERAL (
-			SELECT
-				event_id,
-				sqrt_ratio_after,
-				tick_after,
-				liquidity_after
-			FROM
-				swaps s
-			WHERE
-				pk.pool_key_id = s.pool_key_id
-			ORDER BY
-				event_id DESC
-			LIMIT 1) AS last_swap ON TRUE
-		LEFT JOIN LATERAL (
-			SELECT
-				event_id,
-				sqrt_ratio,
-				tick
-			FROM
-				pool_initializations pi
-			WHERE
-				pi.pool_key_id = pk.pool_key_id
-			ORDER BY
-				event_id DESC
-			LIMIT 1) AS pi ON TRUE),
-	pl AS (
-		SELECT
-			pool_key_id,
-			(
-				SELECT
-					event_id
-				FROM
-					position_updates pu
-				WHERE
-					lss.pool_key_id = pu.pool_key_id
-				ORDER BY
-					event_id DESC
-				LIMIT 1) AS last_update_event_id,
-		(coalesce(liquidity_last, 0::numeric) + coalesce((
-				SELECT
-					sum(liquidity_delta)
-				FROM position_updates AS pu
-				WHERE
-					lss.last_swap_event_id < pu.event_id
-					AND pu.pool_key_id = lss.pool_key_id
-					AND lss.tick BETWEEN pu.lower_bound AND (pu.upper_bound - 1)), 0::numeric)) AS liquidity
-	FROM
-		lss
-)
-	SELECT
-		lss.pool_key_id,
-		sqrt_ratio,
-		tick,
-		liquidity,
-		GREATEST (lss.last_swap_event_id, pl.last_update_event_id) AS last_event_id,
-		pl.last_update_event_id AS last_liquidity_update_event_id
-	FROM
-		lss
-		JOIN pl ON lss.pool_key_id = pl.pool_key_id) AS initial_states;
+	EXECUTE FUNCTION refresh_pool_state ();
