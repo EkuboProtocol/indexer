ad09a9717871155263b2f03d96995d2d2a342a3e
Moody Salem <moody.salem@gmail.com>
2025-11-07 02:46:17 +0800

    improve the tokens migration

--- migrations/018_tokens.sql
+++ migrations/018_tokens.sql
@@ -4,9 +4,11 @@ CREATE TABLE erc20_tokens (
     token_address NUMERIC NOT NULL,
     token_symbol VARCHAR NOT NULL,
     token_name VARCHAR NOT NULL,
-    logo_url TEXT NOT NULL,
+    token_decimals int2 NOT NULL,
     visibility_priority int2 NOT NULL, -- the higher the value, the more priority this token has for being seen
     sort_order int2 NOT NULL, -- the higher the value, the more numerator-like the token is
+    -- extra metadata that is not necessarily populated
+    logo_url TEXT,
     total_supply NUMERIC,
     PRIMARY KEY (chain_id, token_address)
 );
@@ -15,7 +17,8 @@ CREATE TABLE erc20_tokens_bridge_relationships (
     source_chain_id INT8 NOT NULL,
     source_token_address NUMERIC NOT NULL,
     -- the address on the source chain that holds all the bridged tokens
-    source_bridge_address NUMERIC NOT NULL,
+    -- if NULL, it means the token is not "bridged," rather it is minted natively on both chains
+    source_bridge_address NUMERIC,
     dest_chain_id INT8 NOT NULL,
     dest_token_address NUMERIC NOT NULL,
     FOREIGN KEY (source_chain_id, source_token_address) REFERENCES erc20_tokens (chain_id, token_address),
