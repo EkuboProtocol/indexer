b540bbbcc42cd057d83802a0063e4ffe603454e3
Moody Salem <moody.salem@gmail.com>
2025-11-08 18:18:46 +0800

    improve the migrations

--- migrations/030_position_current_liquidity.sql
+++ migrations/030_position_current_liquidity.sql
@@ -1,27 +1,20 @@
 CREATE TABLE position_current_liquidity (
     chain_id int8 NOT NULL,
-    core_address numeric NOT NULL,
+    pool_key_id int8 NOT NULL REFERENCES pool_keys (pool_key_id),
     locker numeric NOT NULL,
     salt numeric NOT NULL,
     lower_bound int4 NOT NULL,
     upper_bound int4 NOT NULL,
     liquidity numeric NOT NULL,
-    PRIMARY KEY (chain_id, core_address, locker, salt, lower_bound, upper_bound)
+    PRIMARY KEY (chain_id, pool_key_id, locker, salt, lower_bound, upper_bound)
 );
 
 CREATE FUNCTION position_current_liquidity_on_insert()
 RETURNS trigger AS $$
-DECLARE
-    v_core_address numeric;
 BEGIN
-    SELECT core_address
-    INTO STRICT v_core_address
-    FROM pool_keys
-    WHERE pool_key_id = NEW.pool_key_id;
-
     INSERT INTO position_current_liquidity (
         chain_id,
-        core_address,
+        pool_key_id,
         locker,
         salt,
         lower_bound,
@@ -29,14 +22,14 @@ BEGIN
         liquidity
     ) VALUES (
         NEW.chain_id,
-        v_core_address,
+        NEW.pool_key_id,
         NEW.locker,
         NEW.salt,
         NEW.lower_bound,
         NEW.upper_bound,
         NEW.liquidity_delta
     )
-    ON CONFLICT (chain_id, core_address, locker, salt, lower_bound, upper_bound)
+    ON CONFLICT (chain_id, pool_key_id, locker, salt, lower_bound, upper_bound)
     DO UPDATE SET
         liquidity = position_current_liquidity.liquidity + EXCLUDED.liquidity;
 
@@ -46,19 +39,12 @@ $$ LANGUAGE plpgsql;
 
 CREATE FUNCTION position_current_liquidity_on_delete()
 RETURNS trigger AS $$
-DECLARE
-    v_core_address numeric;
 BEGIN
-    SELECT core_address
-    INTO STRICT v_core_address
-    FROM pool_keys
-    WHERE pool_key_id = OLD.pool_key_id;
-
     -- it is assumed to exist
     UPDATE position_current_liquidity
     SET liquidity = liquidity - OLD.liquidity_delta
     WHERE chain_id = OLD.chain_id
-      AND core_address = v_core_address
+      AND pool_key_id = OLD.pool_key_id
       AND locker = OLD.locker
       AND salt = OLD.salt
       AND lower_bound = OLD.lower_bound
@@ -74,7 +60,7 @@ $$ LANGUAGE plpgsql;
 
 INSERT INTO position_current_liquidity (
     chain_id,
-    core_address,
+    pool_key_id,
     locker,
     salt,
     lower_bound,
@@ -83,17 +69,16 @@ INSERT INTO position_current_liquidity (
 )
 SELECT
     pu.chain_id,
-    pk.core_address,
+    pu.pool_key_id,
     pu.locker,
     pu.salt,
     pu.lower_bound,
     pu.upper_bound,
     SUM(pu.liquidity_delta) AS liquidity
 FROM position_updates pu
-JOIN pool_keys pk ON pk.pool_key_id = pu.pool_key_id
 GROUP BY
     pu.chain_id,
-    pk.core_address,
+    pu.pool_key_id,
     pu.locker,
     pu.salt,
     pu.lower_bound,
--- migrations/032_nonfungible_token_positions_view.sql
+++ migrations/032_nonfungible_token_positions_view.sql
@@ -1,25 +1,10 @@
 CREATE VIEW nonfungible_token_positions_view AS
-WITH resolved_lockers AS (SELECT n.chain_id,
-                                 n.nft_address,
-                                 n.token_id,
-                                 n.current_owner,
-                                 n.previous_owner,
-                                 n.last_transfer_event_id,
-                                 COALESCE(m.locker, n.nft_address) AS locker
-                          FROM nonfungible_token_owners n
-                                   LEFT JOIN nft_locker_mappings m ON m.chain_id = n.chain_id
-                              AND m.nft_address = n.nft_address)
-SELECT rl.chain_id,
-       rl.nft_address,
-       rl.token_id,
-       rl.current_owner,
-       rl.previous_owner,
-       rl.last_transfer_event_id,
+SELECT n.*,
        pc.core_address,
        pc.lower_bound,
        pc.upper_bound,
        pc.liquidity
-FROM resolved_lockers rl
-         JOIN position_current_liquidity pc ON pc.chain_id = rl.chain_id
-    AND pc.locker = rl.locker
-    AND pc.salt = rl.token_id;
+FROM nonfungible_token_owners n
+         LEFT JOIN nft_locker_mappings m ON m.chain_id = n.chain_id AND m.nft_address = n.nft_address
+         JOIN position_current_liquidity pc
+              ON pc.chain_id = n.chain_id AND pc.locker = COALESCE(m.locker, n.nft_address) AND pc.salt = n.token_id;
