b10eb092fbe699dc115944d161c52242360f479c
Moody Salem <moody.salem@gmail.com>
2025-11-12 06:38:51 +0800

    single file for default tokens

--- scripts/sync-tokens.ts
+++ scripts/sync-tokens.ts
@@ -1,11 +1,6 @@
 import "../src/config";
 import postgres from "postgres";
-import ETH_MAINNET_TOKENS from "./tokens/eth_mainnet.json";
-import ETH_SEPOLIA_TOKENS from "./tokens/eth_sepolia.json";
-import EVM_TOKEN_LOGOS from "./tokens/evm_logos.json";
-import STARKNET_MAINNET_TOKENS from "./tokens/starknet_mainnet.json";
-import STARKNET_SEPOLIA_TOKENS from "./tokens/starknet_sepolia.json";
-import STARKNET_TOKEN_LOGOS from "./tokens/starknet_logos.json";
+import DEFAULT_TOKENS from "./tokens/default-tokens.json";
 
 const sql = postgres(process.env.PG_CONNECTION_STRING, {
   connect_timeout: 5,
@@ -127,114 +122,13 @@ async function main() {
       `Database has ${existingTokensCount[0]?.num_tokens ?? 0} tokens`
     );
 
-    const { count: ethMainnetTokensInserted } = await addTokens(
-      ETH_MAINNET_TOKENS.map((token) => ({
-        chain_id: "1",
-        token_address: token.token_address,
-        token_name: token.name,
-        token_symbol: token.symbol,
-        token_decimals: token.decimals,
-        total_supply:
-          token.total_supply !== null
-            ? BigInt(
-                Math.floor(token.total_supply * 10 ** token.decimals)
-              ).toString()
-            : null,
-        logo_url:
-          (EVM_TOKEN_LOGOS as Record<string, string>)[token.symbol] ?? null,
-        visibility_priority: token.hidden ? -1 : 1,
-        sort_order: token.sort_order,
-        usd_price: null,
-        last_updated: null,
-      })),
+    const { count: defaultTokensInserted } = await addTokens(
+      DEFAULT_TOKENS as Parameters<typeof addTokens>[0],
       true
     );
 
     console.log(
-      `Inserted/updated ${ethMainnetTokensInserted} rows from hard coded list for Ethereum Mainnet`
-    );
-
-    const { count: ethSepoliaTokensInserted } = await addTokens(
-      ETH_SEPOLIA_TOKENS.map((token) => ({
-        chain_id: "11155111",
-        token_address: token.token_address,
-        token_name: token.name,
-        token_symbol: token.symbol,
-        token_decimals: token.decimals,
-        total_supply:
-          token.total_supply !== null
-            ? BigInt(
-                Math.floor(token.total_supply * 10 ** token.decimals)
-              ).toString()
-            : null,
-        logo_url:
-          (EVM_TOKEN_LOGOS as Record<string, string>)[token.symbol] ?? null,
-        visibility_priority: 1,
-        sort_order: token.sort_order,
-        usd_price: null,
-        last_updated: null,
-      })),
-      true
-    );
-
-    console.log(
-      `Inserted/updated ${ethSepoliaTokensInserted} rows from hard coded list for Ethereum Sepolia`
-    );
-
-    const { count: starknetMainnetTokensInserted } = await addTokens(
-      STARKNET_MAINNET_TOKENS.map((token) => ({
-        chain_id: "0x534e5f4d41494e",
-        token_address: token.l2_token_address,
-        token_name: token.name,
-        token_symbol: token.symbol,
-        token_decimals: token.decimals,
-        total_supply:
-          token.total_supply !== null
-            ? BigInt(
-                Math.floor(token.total_supply * 10 ** token.decimals)
-              ).toString()
-            : null,
-        logo_url:
-          (STARKNET_TOKEN_LOGOS as Record<string, string>)[token.symbol] ??
-          null,
-        visibility_priority: 1,
-        sort_order: token.sort_order,
-        usd_price: null,
-        last_updated: null,
-      })),
-      true
-    );
-
-    console.log(
-      `Inserted/updated ${starknetMainnetTokensInserted} rows from hard coded list for starknet mainnet`
-    );
-
-    const { count: starknetSepoliaTokensInserted } = await addTokens(
-      STARKNET_SEPOLIA_TOKENS.map((token) => ({
-        chain_id: "0x534e5f4d41494f",
-        token_address: token.l2_token_address,
-        token_name: token.name,
-        token_symbol: token.symbol,
-        token_decimals: token.decimals,
-        total_supply:
-          token.total_supply !== null
-            ? BigInt(
-                Math.floor(token.total_supply * 10 ** token.decimals)
-              ).toString()
-            : null,
-        logo_url:
-          (STARKNET_TOKEN_LOGOS as Record<string, string>)[token.symbol] ??
-          null,
-        visibility_priority: 1,
-        sort_order: token.sort_order,
-        usd_price: null,
-        last_updated: null,
-      })),
-      true
-    );
-
-    console.log(
-      `Inserted/updated ${starknetSepoliaTokensInserted} rows from hard coded list for starknet mainnet`
+      `Inserted/updated ${defaultTokensInserted} rows from hard coded default token list`
     );
 
     const { count: registrationTokensCount } = await sql`
