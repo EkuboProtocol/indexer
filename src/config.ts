10cf1789a24587c699ad0ca82d01db287fb11c18
Moody Salem <moody.salem@gmail.com>
2026-02-13 12:51:48 -0500

    .env.local should come after the other .locals

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import { config } from "dotenv";
 
-config({ path: `./.env.local` });
 config({
   path: `./.env.${process.env.NETWORK_TYPE}.${process.env.NETWORK}.local`,
 });
 config({ path: `./.env.${process.env.NETWORK_TYPE}.${process.env.NETWORK}` });
 config({ path: `./.env.${process.env.NETWORK_TYPE}.local` });
 config({ path: `./.env.${process.env.NETWORK_TYPE}` });
+config({ path: `./.env.local` });
 config({ path: `./.env` });
